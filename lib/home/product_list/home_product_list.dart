f11dff43dc991db047a4cf7b538048a2e80a5496
connielin <connie@dcard.cc>
2023-04-18 00:18:16 +0800

    fine tuned

--- lib/home/product_list/home_product_list.dart
+++ lib/home/product_list/home_product_list.dart
@@ -2,8 +2,6 @@ import 'package:connie_stylish/home/product_list/home_product_list_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import '../../model/model/product_category.dart';
-import '../../model/repository/ProductRepository.dart';
 import 'home_product_large.dart';
 import 'home_product_small.dart';
 
@@ -11,25 +9,23 @@ class HomeProductList extends StatelessWidget {
   const HomeProductList({
     super.key,
     required this.isLargeScreen,
+    required this.homeProductListCubit,
   });
 
   final bool isLargeScreen;
+  final HomeProductListCubit homeProductListCubit;
 
   @override
   Widget build(BuildContext context) {
-    final cubit = HomeProductListCubit(ProductRepository());
-    cubit.fetch(ProductCategory.values);
-
     return BlocProvider(
-      create: (BuildContext context) => cubit,
+      create: (BuildContext context) => homeProductListCubit,
       child: Builder(builder: (context) => _buildPage(context)),
     );
   }
 
   Widget _buildPage(BuildContext context) {
     return Expanded(
-        child: isLargeScreen
-            ? const LargeHomeList(categories: ProductCategory.values)
-            : const SmallHomeList(categories: ProductCategory.values));
+        child: isLargeScreen ? const LargeHomeList() : const SmallHomeList()
+    );
   }
 }
